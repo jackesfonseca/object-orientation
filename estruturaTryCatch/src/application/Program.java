d35f84905621e80c2ac2fcc77c9746430ac382f3
jackesfonseca <jackesdafonseca@gmail.com>
2020-07-14 10:35:30 -0300

    criacao da=o arquivo para estrutura try catch

---
+++ estruturaTryCatch/src/application/Program.java
@@ -0,0 +1,32 @@
+package application;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Program
+{
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		
+		try
+		{
+			String[] vect = sc.nextLine().split(" ");
+			int position = sc.nextInt();
+			System.out.println(vect[position]);
+		}
+		
+		catch(ArrayIndexOutOfBoundsException e)
+		{
+			System.out.println("Invalid position");
+		}
+		
+		catch(InputMismatchException e)
+		{
+			System.out.println("Input Error");
+		}
+		System.out.println("Fim do programa!");
+		
+		sc.close();
+	}
+}
